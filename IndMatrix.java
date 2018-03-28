f07108da8aca43d7969bbf6c16d6eb062a0a5ff0
Riger73 <s3572290@student.rmit.edu.au>
2018-03-28 14:21:34 +1100

    Reworked from my original code

--- IndMatrix.java
+++ IndMatrix.java
@@ -9,216 +9,104 @@ import java.util.*;
  *
  * @author Jeffrey Chan, 2016.
  */
-public class IndMatrix <T extends Object> implements FriendshipGraph<T>
-{
-
-    private ArrayList<T> Labels;
-    private ArrayList<ArrayList<Byte>> Edges;
-
-
-    /**
-     * Contructs empty graph.
-     */
-    public IndMatrix() {
-        Labels = new ArrayList<T>();
-        Labels = new ArrayList<T>();
-        Edges = new ArrayList<ArrayList<Byte>>();
-    } // end of IndMatrix()
+public class IndMatrixTest <T extends Object> implements FriendshipGraph<T> {
     
-    public void addVertex(T vertLabel) {
-        
-        if (!Labels.contains(vertLabel)){
-            Labels.add(vertLabel);
-            Edges.add(createEmptyArrayList(Edges.size()));
-
-            for (ArrayList<Byte> innerEdges : Edges) {
-                innerEdges.add((byte) 0);
-            }
-
-            display();
-
-        }else{
-            System.out.println("Label already Exists");
-        }
-    } // end of addVertex()
+    private ArrayList<T> incidenceMatrix;
+    private ArrayList<ArrayList<Integer>> rows;
+    private ArrayList<ArrayList<Integer>> cols;
     
+    private int edge;
     
+    public IndMatrixTest() {
+        rows = new ArrayList<ArrayList<Integer>>();
+        cols = new ArrayList<ArrayList<Integer>>();
+        incidenceMatrix = new ArrayList<T>();
+    }
+     
     public void addEdge(T srcLabel, T tarLabel) {
-            
-            int srcIndex = Labels.indexOf(srcLabel);
-            int tarIndex = Labels.indexOf(tarLabel);
-            if (srcIndex == -1 || tarIndex == -1){
-                System.out.printf("\n%s does not Exists", (srcIndex == -1)? srcLabel : tarLabel);
-                return;
-            }
-
-            Edges.get(srcIndex).set(tarIndex, (byte) 1);
-            Edges.get(tarIndex).set(srcIndex, (byte) 1);
-
-            display();
-        } // end of addEdge()
-    
-
-    public ArrayList<T> neighbours(T vertLabel) {
-        ArrayList<T> neighbours = new ArrayList<T>();
-        
-        int index = Labels.indexOf(vertLabel);
-        if (index == -1){
-            System.out.printf("\n%s does not Exists",vertLabel);
-            return neighbours;
-        }
-        
-        for (int i = 0; i < Labels.size(); i++)
-            if (Edges.get(index).get(i) == 1)
-                neighbours.add(Labels.get(i));
-
-
-        return neighbours;
-    } // end of neighbours()
-    
-    
-    public void removeVertex(T vertLabel) {
-        
-        int index = Labels.indexOf(vertLabel);
-
-        if (index == -1){
-            System.out.printf("\n%s does not Exists",vertLabel);
-            return;
-        }
-
-        //Remove from the edges list;
-        
-        Edges.remove(index);
-        for (ArrayList<Byte> innerEdges : Edges) {
-            innerEdges.remove(index);
-        }
-
-        Labels.remove(index);
-
-        display();
-    } // end of removeVertex()
-    
-    
-    public void removeEdge(T srcLabel, T tarLabel) {
-        
-        int srcIndex = Labels.indexOf(srcLabel);
-        int tarIndex = Labels.indexOf(tarLabel);
+        int srcIndex = incidenceMatrix.indexOf(srcLabel);
+        int tarIndex = incidenceMatrix.indexOf(tarLabel);
         if (srcIndex == -1 || tarIndex == -1){
             System.out.printf("\n%s does not Exists", (srcIndex == -1)? srcLabel : tarLabel);
             return;
         }
 
-        Edges.get(srcIndex).set(tarIndex, (byte) 0);
-        Edges.get(tarIndex).set(srcIndex, (byte) 0);
+        cols.get(srcIndex).set(tarIndex, (Integer) 1);
+        cols.get(tarIndex).set(srcIndex, (Integer) 1);
 
+        display();
 
-    } // end of removeEdges()
-    
-    
-    public void printVertices(PrintWriter os) {
-        
-        for (T label : Labels) {
-            os.printf("%s ", label);
+    }
+
+    public int getEdge(int cols, int rows) 
+    {
+        try {
+            return incidenceMatrix.indexOf(rows);
+        } catch (ArrayIndexOutOfBoundsException index) {
+            System.out.println("The vertices does not exists");
         }
-    } // end of printVertices()
+        return -1;
+    }
     
-    
-    public void printEdges(PrintWriter os) {
-        
-        for (int i = 0; i < Labels.size(); i++){
-
-            for (int j = 0; j < Labels.size(); j++){
-                if (Edges.get(i).get(j) == (byte) 1)
-                    os.printf("%s %s\n",Labels.get(i),Labels.get(j));
-            }
-        }
-
-
-    } // end of printEdges()
-    
-    
-    public int shortestPathDistance(T vertLabel1, T vertLabel2) {
-        // Implement me!
-    
-        boolean[] checked = new boolean[Labels.size()];
-        int[] distances = new int[Labels.size()];
-        
-        Queue <T> queue = new LinkedList<T>();
-
-        T curVert = vertLabel1;
-        int curIndex = Labels.indexOf(vertLabel1);
-
-        int wantedIndex = Labels.indexOf(vertLabel2);
-
-
-        if (curIndex == wantedIndex)
-            return 0;
-
-        while (curVert != null){
-
-            if (!checked[curIndex]){
-                checked[curIndex] = true;
-
-                for(T nVert : neighbours(curVert)){
-                    int nIndex = Labels.indexOf(nVert);
-
-                    if (checked[nIndex])
-                        continue;
-
-                    System.out.printf("  - Checking %s: %s", curVert,nVert);
-
-                    if (nIndex == wantedIndex){
-                        System.out.printf("  (%s = %s)\n",nVert, vertLabel2);
-                        return distances[curIndex] + 1;
-                    }else {
-                        System.out.printf("  (%s != %s)\n",nVert, vertLabel2);
-                        distances[nIndex] = distances[curIndex] + 1;
-                        queue.add(nVert);
-                    }
-                } //end For
-            } //end (if !checked)
-        
-            curVert = queue.poll();
-            curIndex = Labels.indexOf(curVert);
-        }//end While
-
-        // if we reach this point, source and target are disconnected
-        return disconnectedDist;        
-    } // end of shortestPathDistance()
-
-
-    private void display(){      
+    private void display() {
         System.out.printf("\n   "); 
         // Print Column (edge) headings
-        for (int i = 0; i < Labels.size(); i++){
-
-            for (int j = 0; j < Labels.size(); j++){
-                if (Edges.get(i).get(j) == (byte) 1)
-                    System.out.printf("E%s  ", Labels.get(i),Labels.get(j));
-            }
+        for (T label : incidenceMatrix) {
+            System.out.printf("E%s  ", label);
         }
         // Print Row (vertices) headings
-        for (int row = 0; row < Edges.size(); row++){
-            System.out.printf("\nV%s |", Labels.get(row));
+        for (int i = 0; i < rows.size(); i++){
+            System.out.printf("\nV%s |", incidenceMatrix.get(i));
             // Print result - 0 if not incident and 1 if incident     
-            for (int i = 0; i < Labels.size(); i++)
-            {      
-                System.out.printf("%s, ", neighbours(Labels.get(i)).contains(Labels.get(row)) ? 1 : 0);
+            for (Integer edge : rows.get(i)) {      
+                System.out.printf("%s, ", neighbours(incidenceMatrix.get(edge)).contains(edge) ? 1 : 0);
             }
             System.out.printf("|");
             
         } 
         System.out.printf("\n"); 
-    } 
-
-    private ArrayList<Byte> createEmptyArrayList(int size){
-
-        ArrayList<Byte> retVal = new ArrayList<Byte>();
-
-        while(retVal.size() < size)
-            retVal.add((byte)0);
-
-        return retVal;
     }
-    
-} // end of class IndMatrix
+
+    @Override
+    public void addVertex(T vertLabel) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public ArrayList<T> neighbours(T vertLabel) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public void removeVertex(T vertLabel) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void removeEdge(T srcLabel, T tarLabel) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void printVertices(PrintWriter os) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void printEdges(PrintWriter os) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public int shortestPathDistance(T vertLabel1, T vertLabel2) {
+        // TODO Auto-generated method stub
+        return 0;
+    } 
+        
+}
