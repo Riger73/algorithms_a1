208824f1a455d7d6e6259931841ffb2f5af3288e
Riger73 <s3572290@student.rmit.edu.au>
2018-04-08 21:32:54 +1000

    Rebuilt using Array instead of ArrayList
    Not yet tested

--- IndMatrix.java
+++ IndMatrix.java
@@ -12,213 +12,285 @@ import java.util.*;
 public class IndMatrix <T extends Object> implements FriendshipGraph<T>
 {
 
-    private ArrayList<T> Labels;
-    private ArrayList<ArrayList<Byte>> Edges;
-
-
+	private static final int VERTSIZE = 100;
+	private static final int EDGESIZE = 100;
+	
+    Object[][] edges;
+    Object[] vertices;
+    int vIndexFree = -1;
+    
     /**
-     * Contructs empty graph.
-     */
+	 * Constructs empty graph.
+	 */
     public IndMatrix() {
-        Labels = new ArrayList<T>();
-        Labels = new ArrayList<T>();
-        Edges = new ArrayList<ArrayList<Byte>>();
+    	// Implement me!
+    	// Initialize empty vertices
+    	vertices = new Object[VERTSIZE];
+    	//array of array of edge
+    	edges = new Object[vertices.length][];
     } // end of IndMatrix()
     
-    public void addVertex(T vertLabel) {
-        
-        if (!Labels.contains(vertLabel)){
-            Labels.add(vertLabel);
-            Edges.add(createEmptyArrayList(Edges.size()));
-
-            for (ArrayList<Byte> innerEdges : Edges) {
-                innerEdges.add((byte) 0);
-            }
-
-            display();
-
-        }else{
-            System.out.println("Label already Exists");
-        }
-    } // end of addVertex()
+    /**
+     * Allows the vertices array to grow "dynamically"
+     */
+    private void growVertArray(){
+    	Object[][] oldEdges = edges;
+    	Object[] oldVertices = vertices;
+    	//create a bigger array of vertLabel
+    	vertices = new Object[vertices.length + VERTSIZE];
+    	edges = new Object[vertices.length][];
+    	//copy vertLabel data from old to new
+    	System.arraycopy(oldVertices, 0, vertices, 0, oldVertices.length);
+    	System.arraycopy(oldEdges, 0, edges, 0, oldEdges.length);
+    	vIndexFree = oldVertices.length;
+    }
+    /**
+     * Allows the edge array to grow "dynamically"
+     * @param vertLabelIndex
+     */
+    private int growEdgeArray(int vertLabelIndex){
+    	int eIndexFree = -1;
+    	if(vertLabelIndex < vertices.length){
+    		Object[] oldEdgeArray = edges[vertLabelIndex];
+    		if(oldEdgeArray == null){
+    			edges[vertLabelIndex] = new Object[EDGESIZE];
+    			eIndexFree = 0;
+    		}else{
+    			edges[vertLabelIndex] = new Object[edges[vertLabelIndex].length + EDGESIZE];
+    			System.arraycopy(oldEdgeArray, 0, edges[vertLabelIndex], 0, oldEdgeArray.length);
+    			eIndexFree = oldEdgeArray.length;
+    		}
+    		
+    	}
+    	return eIndexFree;
+    }
     
+    private boolean isVertArrayFull(){
+    	boolean full = true;
+    	// check the removed Vertex Index
+    	if(vIndexFree != -1){
+    		full = false;
+    	}else{
+    		//loop the array of vertices and check for nulls
+    		for(int i = 0 ; i < vertices.length ; i ++){
+        		Object o = vertices[i];
+    			if(o == null){
+    				full = false;
+    				vIndexFree = i;
+    				break;
+    			}
+    		}
+    	}
+    	return full;
+    }
+    
+    /**
+     * index for new edges
+     * @param vertLabel
+     * @return
+     */
+    private int getFreeEdgeIndex(int vertLabel,Object newEdge){
+    	int edgeIndex = -1;
+    	boolean exist = false;
+    	if(vertLabel < vertices.length){
+    		Object[] edgeConnect = edges[vertLabel]; 
+    		if(edgeConnect != null){
+    			for(int i = 0 ; i < edgeConnect.length ; i ++){
+    				if(edgeIndex == -1 && edgeConnect[i] == null){
+    					edgeIndex = i;
+    				}
+    				if(edgeConnect[i] != null && edgeConnect[i].equals(newEdge)){
+    					exist = true;
+    					break;
+    				}
+    			}
+    		}
+    		//checks for empty then adds to the array
+    		if(edgeIndex == -1 && !exist) edgeIndex = growEdgeArray(vertLabel);
+    	}
+    	return edgeIndex;
+    }
+    
+    private int getEdgeIndex(int vertLabelIndex , Object edge){
+    	int index = -1;
+    	Object[] edgeConnect = edges[vertLabelIndex]; 
+    	if(edgeConnect != null){
+    		for(int i = 0; i < edgeConnect.length; i ++){
+    			Object currentEdge = edgeConnect[i];
+    			if(currentEdge != null && currentEdge.equals(edge)){
+    				index = i;
+    				break;
+    			}
+    		}
+    	}
+    	return index;
+    }
+    
+    /**
+     * get the index of vertLabel in array
+     * @param v
+     * @return
+     */
+    private int vertIndex(Object v){
+    	int index = -1;
+    	for(int i = 0 ; i < vertices.length ; i ++){
+    		Object o = vertices[i];
+			if(o != null && o.equals(v)){
+				index = i;
+				break;
+			}
+		}
+    	return index;
+    }
+    
+    public void addVertex(T vertLabel) {
+        // Implement me!
+    	// checks for empty then adds to the array
+    	if(vertLabel !=null && vertIndex(vertLabel) == -1){
+    		if(isVertArrayFull()){
+    			growVertArray();
+    		}
+			vertices[vIndexFree] = vertLabel;
+			edges[vIndexFree] = new Object[VERTSIZE];
+			vIndexFree = -1;
+    	}
+    } // end of addVertex()
+	
     
     public void addEdge(T srcLabel, T tarLabel) {
-            
-            int srcIndex = Labels.indexOf(srcLabel);
-            int tarIndex = Labels.indexOf(tarLabel);
-            if (srcIndex == -1 || tarIndex == -1){
-                System.out.printf("\n%s does not Exists", (srcIndex == -1)? srcLabel : tarLabel);
-                return;
-            }
-
-            Edges.get(srcIndex).set(tarIndex, (byte) 1);
-            Edges.get(tarIndex).set(srcIndex, (byte) 1);
-
-            display();
-        } // end of addEdge()
-    
+        // Implement me!
+    	// finds valid vertLabel nodes for edge
+    	if(srcLabel != null && tarLabel !=null && !srcLabel.equals(tarLabel)){
+    		int srcLabelIndex = vertIndex(srcLabel);
+    		int tarLabelIndex = vertIndex(tarLabel);
+    		if(srcLabelIndex != -1 && tarLabelIndex != -1){
+    			// add new edge to edge array
+    			int edgeIndex1 = getFreeEdgeIndex(srcLabelIndex,tarLabel);
+    			// Checks for duplicate edges
+    			if(edgeIndex1 == -1) return;
+    			    int edgeIndex2 = getFreeEdgeIndex(tarLabelIndex,srcLabel);
+    			if(edgeIndex2 == -1 )return;
+    			    edges[srcLabelIndex][edgeIndex1] = tarLabel;
+    			    edges[tarLabelIndex][edgeIndex2] = srcLabel;
+    		}else{
+    			throw new IllegalArgumentException();
+    		}
+    		
+    	}
+    } // end of addEdge()
+	
 
     public ArrayList<T> neighbours(T vertLabel) {
         ArrayList<T> neighbours = new ArrayList<T>();
+        // Implement me!
+        int vertIndex;
+        // checks if vertLabel exists
+        if(vertLabel != null && ((vertIndex=this.vertIndex(vertLabel)) != -1)){
+        	// identify neighbours and then add them to neighbours ArrayList
+        	for(int i = 0; i < edges[vertIndex].length; i ++){
+        		T t = (T)edges[vertIndex][i];
+        		if(t != null){
+        			neighbours.add(t);
+        		}
+        	}
+        }else{
+			throw new IllegalArgumentException();
+		}
         
-        int index = Labels.indexOf(vertLabel);
-        if (index == -1){
-            System.out.printf("\n%s does not Exists",vertLabel);
-            return neighbours;
-        }
-        
-        for (int i = 0; i < Labels.size(); i++)
-            if (Edges.get(index).get(i) == 1)
-                neighbours.add(Labels.get(i));
-
-
         return neighbours;
     } // end of neighbours()
     
     
     public void removeVertex(T vertLabel) {
-        
-        int index = Labels.indexOf(vertLabel);
-
-        if (index == -1){
-            System.out.printf("\n%s does not Exists",vertLabel);
-            return;
-        }
-
-        //Remove from the edges list;
-        
-        Edges.remove(index);
-        for (ArrayList<Byte> innerEdges : Edges) {
-            innerEdges.remove(index);
-        }
-
-        Labels.remove(index);
-
-        display();
+        // Implement me!
+        int vertIndex;
+        // check if vertLabel exists
+        if(vertLabel!=null && ((vertIndex=this.vertIndex(vertLabel)) != -1)){
+        	// remove vertLabel
+        	vertices[vertIndex] = null;
+        	Object[] oldEdges = edges[vertIndex];
+        	edges[vertIndex] = null;
+        	// find and remove all edges incident the vertLabel
+        	for(int i = 0 ; i < oldEdges.length ; i ++){
+        		if(oldEdges[i]!=null){
+        			int tempIndex = vertIndex(oldEdges[i]);
+            		int tempIndex2 = getEdgeIndex(tempIndex,vertLabel);
+            		if(tempIndex!=-1 && tempIndex2!=-1){
+            			edges[tempIndex][tempIndex2] = null;
+            		}
+        		}
+        	}
+        	this.vIndexFree = vertIndex;
+        }else{
+			throw new IllegalArgumentException();
+		}
     } // end of removeVertex()
-    
+	
     
     public void removeEdge(T srcLabel, T tarLabel) {
-        
-        int srcIndex = Labels.indexOf(srcLabel);
-        int tarIndex = Labels.indexOf(tarLabel);
-        if (srcIndex == -1 || tarIndex == -1){
-            System.out.printf("\n%s does not Exists", (srcIndex == -1)? srcLabel : tarLabel);
-            return;
-        }
-
-        Edges.get(srcIndex).set(tarIndex, (byte) 0);
-        Edges.get(tarIndex).set(srcIndex, (byte) 0);
-
-
+        // Implement me!
+    	// checks for removal conditions: null & equal
+    	if(srcLabel != null && tarLabel !=null && !srcLabel.equals(tarLabel)){
+    		// Checks that edge & nodes exit
+    		int srcLabelIndex = vertIndex(srcLabel);
+    		int tarLabelIndex = vertIndex(tarLabel);
+    		// Checks if vertices are present in array: removeable
+    		if(srcLabelIndex != -1 && tarLabelIndex != -1){
+    			int edgeIndex1 = getEdgeIndex(srcLabelIndex, tarLabel);
+    			if(edgeIndex1 == -1) return; 
+    			int edgeIndex2 = getEdgeIndex(tarLabelIndex,srcLabel);
+    			if(edgeIndex2 == -1 )return; 
+    			// Remove edges
+    			edges[srcLabelIndex][edgeIndex1] = null;
+    			edges[tarLabelIndex][edgeIndex2] = null;
+    		}else{
+    			throw new IllegalArgumentException();
+    		}
+    		
+    	}
     } // end of removeEdges()
-    
-    
+	
+    /**
+     * print all vertices
+     */
     public void printVertices(PrintWriter os) {
-        
-        for (T label : Labels) {
-            os.printf("%s ", label);
-        }
+        // Implement me!
+    	// iterate through array elements and print
+    	for(int i = 0 ; i < vertices.length ; i ++){
+    		Object vertLabel = vertices[i];
+    		if(vertLabel != null){
+    			os.print(vertLabel.toString() + " ");
+    		}
+    	}
+    	os.println();
+    	
     } // end of printVertices()
-    
-    
+	
+    /**
+     * print all edges
+     */
     public void printEdges(PrintWriter os) {
-        
-        for (int i = 0; i < Labels.size(); i++){
-
-            for (int j = 0; j < Labels.size(); j++){
-                if (Edges.get(i).get(j) == (byte) 1)
-                    os.printf("%s %s\n",Labels.get(i),Labels.get(j));
-            }
-        }
-
-
+        // Implement me!
+     	// iterate through array elements and print
+    	for(int i = 0; i < vertices.length; i ++){
+    		Object vertLabel = vertices[i];
+    		if(vertLabel != null && edges[i] != null){
+    			for(int j = 0; j < edges[i].length; j ++){
+    				if(edges[i][j]!=null){
+    					os.println(vertLabel.toString() + " " + edges[i][j].toString());
+    				}
+    			}
+    		}
+    	}
+	
     } // end of printEdges()
     
     
-    public int shortestPathDistance(T vertLabel1, T vertLabel2) {
-        // Implement me!
-    
-        boolean[] checked = new boolean[Labels.size()];
-        int[] distances = new int[Labels.size()];
-        
-        Queue <T> queue = new LinkedList<T>();
+    //public int shortestPathDistance(T vertLabel1, T vertLabel2) {
+    	// Implement me!
 
-        T curVert = vertLabel1;
-        int curIndex = Labels.indexOf(vertLabel1);
-
-        int wantedIndex = Labels.indexOf(vertLabel2);
-
-
-        if (curIndex == wantedIndex)
-            return 0;
-
-        while (curVert != null){
-
-            if (!checked[curIndex]){
-                checked[curIndex] = true;
-
-                for(T nVert : neighbours(curVert)){
-                    int nIndex = Labels.indexOf(nVert);
-
-                    if (checked[nIndex])
-                        continue;
-
-                    System.out.printf("  - Checking %s: %s", curVert,nVert);
-
-                    if (nIndex == wantedIndex){
-                        System.out.printf("  (%s = %s)\n",nVert, vertLabel2);
-                        return distances[curIndex] + 1;
-                    }else {
-                        System.out.printf("  (%s != %s)\n",nVert, vertLabel2);
-                        distances[nIndex] = distances[curIndex] + 1;
-                        queue.add(nVert);
-                    }
-                } //end For
-            } //end (if !checked)
-        
-            curVert = queue.poll();
-            curIndex = Labels.indexOf(curVert);
-        }//end While
-
-        // if we reach this point, source and target are disconnected
-        return disconnectedDist;        
-    } // end of shortestPathDistance()
-
-
-    private void display(){      
-        System.out.printf("\n   "); 
-        // Print Column (edge) headings
-        for (int i = 0; i < Labels.size(); i++){
-
-            for (int j = 0; j < Labels.size(); j++){
-                if (Edges.get(i).get(j) == (byte) 1)
-                    System.out.printf("E%s  ", Labels.get(i),Labels.get(j));
-            }
-        }
-        // Print Row (vertices) headings
-        for (int row = 0; row < Edges.size(); row++){
-            System.out.printf("\nV%s |", Labels.get(row));
-            // Print result - 0 if not incident and 1 if incident     
-            for (int i = 0; i < Labels.size(); i++)
-            {      
-                System.out.printf("%s, ", neighbours(Labels.get(i)).contains(Labels.get(row)) ? 1 : 0);
-            }
-            System.out.printf("|");
-            
-        } 
-        System.out.printf("\n"); 
-    } 
-
-    private ArrayList<Byte> createEmptyArrayList(int size){
-
-        ArrayList<Byte> retVal = new ArrayList<Byte>();
-
-        while(retVal.size() < size)
-            retVal.add((byte)0);
-
-        return retVal;
-    }
+    	// if we reach this point, source and target are disconnected
+        // return shortestPathDistance;    	
+    //} // end of shortestPathDistance()	
     
 } // end of class IndMatrix
