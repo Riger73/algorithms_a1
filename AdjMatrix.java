d27de733b49c17bb67e744aa623b253812fc81aa
Joshua Reason <s3544917@student.rmit.edu.au>
2018-04-11 13:07:39 +1000

    added comments

--- AdjMatrix.java
+++ AdjMatrix.java
@@ -13,6 +13,7 @@ public class AdjMatrix <T extends Object> implements FriendshipGraph<T>
 {
 
 
+    //Arrays to hold data
     private ArrayList<T> Labels;
     private ArrayList<ArrayList<Byte>> Edges;
 
@@ -21,6 +22,7 @@ public class AdjMatrix <T extends Object> implements FriendshipGraph<T>
 	 * Contructs empty graph.
 	 */
     public AdjMatrix() {
+        //instantiate arays
         Labels = new ArrayList<T>();
         Edges = new ArrayList<ArrayList<Byte>>();
     } // end of AdjMatrix()
@@ -28,17 +30,23 @@ public class AdjMatrix <T extends Object> implements FriendshipGraph<T>
     
     public void addVertex(T vertLabel) {
         
+        //Check if label doesn't exist in array already
         if (!Labels.contains(vertLabel)){
+
+            //add to Label Array
             Labels.add(vertLabel);
+
+            //Adds a new inner array size of the outer array (Adds new Row)
             Edges.add(createEmptyArrayList(Edges.size()));
 
+            //loop through each of the inner arrays and adds a new entry to it (adds new colum)
             for (ArrayList<Byte> innerEdges : Edges) {
                 innerEdges.add((byte) 0);
             }
 
             printMatrix();
 
-        }else{
+        }else{ //else print that the vertex already exits
             System.out.println("Label already Exists");
         }
     } // end of addVertex()
@@ -46,6 +54,7 @@ public class AdjMatrix <T extends Object> implements FriendshipGraph<T>
     
     public void addEdge(T srcLabel, T tarLabel) {
         
+        //checks if labels exist in graph
         int srcIndex = Labels.indexOf(srcLabel);
         int tarIndex = Labels.indexOf(tarLabel);
         if (srcIndex == -1 || tarIndex == -1){
@@ -53,6 +62,7 @@ public class AdjMatrix <T extends Object> implements FriendshipGraph<T>
             return;
         }
 
+        //sets rows and columns in graph to one
         Edges.get(srcIndex).set(tarIndex, (byte) 1);
         Edges.get(tarIndex).set(srcIndex, (byte) 1);
 
@@ -61,39 +71,49 @@ public class AdjMatrix <T extends Object> implements FriendshipGraph<T>
 	
 
     public ArrayList<T> neighbours(T vertLabel) {
+
+        //instatiate array to return
         ArrayList<T> neighbours = new ArrayList<T>();
         
+        //get index of label 
         int index = Labels.indexOf(vertLabel);
+
+        //if doesn't exist return
         if (index == -1){
             System.out.printf("\n%s does not Exists",vertLabel);
             return neighbours;
         }
         
+        //iterate through row of index adding edges
         for (int i = 0; i < Labels.size(); i++)
             if (Edges.get(index).get(i) == 1)
                 neighbours.add(Labels.get(i));
 
-
+        //return
         return neighbours;
     } // end of neighbours()
     
     
     public void removeVertex(T vertLabel) {
         
+        //get index of label
         int index = Labels.indexOf(vertLabel);
 
+        //if doesn't exist return
         if (index == -1){
             System.out.printf("\n%s does not Exists",vertLabel);
             return;
         }
-
-        //Remove from the edges list;
-        
+       
+        //remove outer array of vert (row)
         Edges.remove(index);
+
+        //iterate through inner arrays and removes vert (column)
         for (ArrayList<Byte> innerEdges : Edges) {
             innerEdges.remove(index);
         }
 
+        //remove label from label array
         Labels.remove(index);
 
         printMatrix();
@@ -102,6 +122,7 @@ public class AdjMatrix <T extends Object> implements FriendshipGraph<T>
     
     public void removeEdge(T srcLabel, T tarLabel) {
         
+        //checks if labels exist in graph
         int srcIndex = Labels.indexOf(srcLabel);
         int tarIndex = Labels.indexOf(tarLabel);
         if (srcIndex == -1 || tarIndex == -1){
@@ -109,6 +130,7 @@ public class AdjMatrix <T extends Object> implements FriendshipGraph<T>
             return;
         }
 
+        //set rows and columns to zero
         Edges.get(srcIndex).set(tarIndex, (byte) 0);
         Edges.get(tarIndex).set(srcIndex, (byte) 0);
 
